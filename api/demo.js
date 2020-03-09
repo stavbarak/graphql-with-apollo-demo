3342b61267b381f4370d2d5563d1fd767b84d6b3
Stav Barak <stavb@tikalk.com>
2020-03-09 11:47:34 +0200

    empty demo

--- api/demo.js
+++ api/demo.js
@@ -2,30 +2,10 @@ const gql = require('graphql-tag');
 const { ApolloServer } = require('apollo-server');
 
 // typeDefs
-const typeDefs = gql`
-type User {
-    email: String!
-    avatar: String!
-    friends: [User]!
-}
 
-type Query {
-    me: User!
-}
-`
 
 // resolvers
-const resolvers = {
-    Query: {
-        me() {
-            return {
-                email: "bla",
-                avatar: "bla",
-                friends: []
-            }
-        }
-    }
-}
+
 
 const server = new ApolloServer({
     typeDefs,
